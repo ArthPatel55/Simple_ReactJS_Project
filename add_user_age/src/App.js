e38ea77b8a0e6bf1cc6ca2914264547f0563396f
Arth H. Patel <arth.patel@bypeopletechnologies.com>
2024-02-14 12:36:46 +0530

    add user list

--- add_user_age/src/App.js
+++ add_user_age/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddUser from './components/User/AddUser';
+import UserList from './components/User/UserList';
 
 
 function App() {
   return (
     <>
     <AddUser/>
+    <UserList users={[]}/>
     </>
   );
 }
