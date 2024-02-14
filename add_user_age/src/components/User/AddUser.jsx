6cf370e79007a676bd26c4459a9e275182ecaf92
Arth H. Patel <arth.patel@bypeopletechnologies.com>
2024-02-14 11:53:04 +0530

    manage useState for user input

--- add_user_age/src/components/User/AddUser.jsx
+++ add_user_age/src/components/User/AddUser.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
-import Card from '../UI/Card';
-import classes from './AddUser.module.css'
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
-  const addUserHandler =(event)=>{
-    event.preventDefault();
-  }
-  return (<>
-   <Card className={classes.input} >
-  <form onSubmit={addUserHandler}>
-    <label htmlFor="username" >User Name</label>
-    <input type="text" id="username" />
-    <label htmlFor="age" >Age (Year)</label>
-    <input type="number" id="age" />
-    <button type="submit" >Add User</button>
-  </form>
-  </Card>
-  </>
-  )
-}
+  const [enteredUsername ,setEnteredUsername] = useState('');
+  const [enteredAge,setEnteredAge] = useState('');
 
-export default AddUser
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredUsername,enteredAge)
+  };
+  const usernameChangeHandler =(event)=>{
+    setEnteredUsername(event.target.value);
+  }
+  const ageChangeHandler = (event)=>{
+    setEnteredAge(event.target.value);
+  }
+  return (
+    <>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">User Name</label>
+          <input type="text" id="username" onChange={usernameChangeHandler} />
+          <label htmlFor="age">Age (Year)</label>
+          <input type="number" id="age" onChange={ageChangeHandler} />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </>
+  );
+};
+
+export default AddUser;
